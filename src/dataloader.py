db0b9962a1137e6329bf2088654f55e84353a1dd
ArchiMickey <wuchunyat119@gmail.com>
2022-10-04 11:23:11 +0800

    add lightning dm

--- src/dataloader.py
+++ src/dataloader.py
@@ -1,3 +1,4 @@
+import pytorch_lightning as pl
 from os import listdir
 from os.path import join
 
@@ -5,21 +6,22 @@ from PIL import Image
 from torch.utils.data.dataset import Dataset
 from torchvision.transforms import Compose, RandomCrop, ToTensor, ToPILImage, CenterCrop, Resize
 
+from loguru import logger
+from icecream import ic
 
+
+# Helper function for image preprocessing
 def is_image_file(filename):
     return any(filename.endswith(extension) for extension in ['.png', '.jpg', '.jpeg', '.PNG', '.JPG', '.JPEG'])
 
-
 def calculate_valid_crop_size(crop_size, upscale_factor):
     return crop_size - (crop_size % upscale_factor)
-
 
 def train_hr_transform(crop_size):
     return Compose([
         RandomCrop(crop_size),
         ToTensor(),
     ])
-
 
 def train_lr_transform(crop_size, upscale_factor):
     return Compose([
@@ -38,61 +40,31 @@ def display_transform():
     ])
 
 
-class TrainDatasetFromFolder(Dataset):
-    def __init__(self, dataset_dir, crop_size, upscale_factor):
-        super(TrainDatasetFromFolder, self).__init__()
-        self.image_filenames = [join(dataset_dir, x) for x in listdir(dataset_dir) if is_image_file(x)]
-        crop_size = calculate_valid_crop_size(crop_size, upscale_factor)
-        self.hr_transform = train_hr_transform(crop_size)
-        self.lr_transform = train_lr_transform(crop_size, upscale_factor)
+class SRGANDataModule(pl.LightningDataModule):
+    def __init__(self, cfg) -> None:
+        super().__init__()
+        self.crop_size = cfg['dm']['crop_size']
+        self.upscale_factor = cfg['dm']['upscale_factor']
+        self.datapath = cfg['datapath']
+        self.train_datapath = self.datapath['train']
+        self.val_datapath = self.datapath['val']
+        ic(self.crop_size, self.upscale_factor, self.datapath, self.train_datapath, self.val_datapath)
+        
+        self.train_hr_transform = train_hr_transform(self.crop_size)
+        self.train_lr_transform = train_lr_transform(self.crop_size, self.upscale_factor)        
+    
+    def setup(self, stage: str) -> None:
+        if stage == 'fit':
+            self.srgan_train = []
+            self.srgan_val = []
+            
+            for path in self.train_datapath:
+                self.srgan_train += [join(path, x) for x in listdir(path) if is_image_file(x)]
+            
+            for path in self.val_datapath:
+                self.srgan_val += [join(path, x) for x in listdir(path) if is_image_file(x)]
 
-    def __getitem__(self, index):
-        hr_image = self.hr_transform(Image.open(self.image_filenames[index]))
-        lr_image = self.lr_transform(hr_image)
-        return lr_image, hr_image
-
-    def __len__(self):
-        return len(self.image_filenames)
-
-
-class ValDatasetFromFolder(Dataset):
-    def __init__(self, dataset_dir, upscale_factor):
-        super(ValDatasetFromFolder, self).__init__()
-        self.upscale_factor = upscale_factor
-        self.image_filenames = [join(dataset_dir, x) for x in listdir(dataset_dir) if is_image_file(x)]
-
-    def __getitem__(self, index):
-        hr_image = Image.open(self.image_filenames[index])
-        w, h = hr_image.size
-        crop_size = calculate_valid_crop_size(min(w, h), self.upscale_factor)
-        lr_scale = Resize(crop_size // self.upscale_factor, interpolation=Image.BICUBIC)
-        hr_scale = Resize(crop_size, interpolation=Image.BICUBIC)
-        hr_image = CenterCrop(crop_size)(hr_image)
-        lr_image = lr_scale(hr_image)
-        hr_restore_img = hr_scale(lr_image)
-        return ToTensor()(lr_image), ToTensor()(hr_restore_img), ToTensor()(hr_image)
-
-    def __len__(self):
-        return len(self.image_filenames)
-
-
-class TestDatasetFromFolder(Dataset):
-    def __init__(self, dataset_dir, upscale_factor):
-        super(TestDatasetFromFolder, self).__init__()
-        self.lr_path = dataset_dir + '/SRF_' + str(upscale_factor) + '/data/'
-        self.hr_path = dataset_dir + '/SRF_' + str(upscale_factor) + '/target/'
-        self.upscale_factor = upscale_factor
-        self.lr_filenames = [join(self.lr_path, x) for x in listdir(self.lr_path) if is_image_file(x)]
-        self.hr_filenames = [join(self.hr_path, x) for x in listdir(self.hr_path) if is_image_file(x)]
-
-    def __getitem__(self, index):
-        image_name = self.lr_filenames[index].split('/')[-1]
-        lr_image = Image.open(self.lr_filenames[index])
-        w, h = lr_image.size
-        hr_image = Image.open(self.hr_filenames[index])
-        hr_scale = Resize((self.upscale_factor * h, self.upscale_factor * w), interpolation=Image.BICUBIC)
-        hr_restore_img = hr_scale(lr_image)
-        return image_name, ToTensor()(lr_image), ToTensor()(hr_restore_img), ToTensor()(hr_image)
-
-    def __len__(self):
-        return len(self.lr_filenames)
+if __name__ == '__main__':
+    from loguru import logger
+    from icecream import ic
+    dm = SRGANDataModule()
